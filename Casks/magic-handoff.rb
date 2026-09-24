d7a012a17e20249e213a2b918d33a4fa9fa7615e
Bekir Ersever <bekirersever@outlook.com>
2026-09-25 01:14:35 +0300

    magic-handoff 0.0.7 (20)

--- Casks/magic-handoff.rb
+++ Casks/magic-handoff.rb
@@ -2,8 +2,7 @@ cask "magic-handoff" do
   version "0.0.7,20"
   sha256 "2dc6e23b0c5846ed38748807a9af5d1283a54f1df66e5821c07ca54ba5a094be"
 
-  url "https://github.com/bekir1184/magic-handoff/releases/download/v#{version.csv.first}/Magic-Handoff-#{version.csv.first}-#{version.csv.second}.zip",
-      verified: "github.com/bekir1184/magic-handoff/"
+  url "https://github.com/bekir1184/magic-handoff/releases/download/v#{version.csv.first}/Magic-Handoff-#{version.csv.first}-#{version.csv.second}.zip"
   name "Magic Handoff"
   desc "Moves Magic Keyboard, Trackpad and Mouse between two Macs with one keystroke"
   homepage "https://github.com/bekir1184/magic-handoff"
@@ -13,7 +12,7 @@ cask "magic-handoff" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Magic Handoff.app"
 
